2e166e26f230b30188e6e405b32be99fd4258209
igor <igor.nigay@gmail.com>
2015-12-16 22:09:26 -0800

    Adds Basic markup and Comparison Functions

---
+++ CalculateDifference.php
@@ -0,0 +1,98 @@
+<?php
+
+const FILE1 = "file1.csv";
+const FILE2 = "file2.csv";
+
+try{
+
+	if ( move_uploaded_file ( $_FILES["file1"]["tmp_name"] , 
+	     "uploads/" . FILE2) && move_uploaded_file ( $_FILES["file2"]["tmp_name"] , 
+	     "uploads/" . FILE2)){
+
+	  	$handle1 = fopen("uploads/" . FILE1, "r");
+		$handle2 = fopen("uploads/" . FILE2, "r");
+	   if( $handle1 != false && $handle2 != false){
+
+
+	   		//$h = new DiffChecker();
+	   		//var_dump($h->checkSubscriber($str1, $str2));
+	   		$batch = new CompareBatch($handle1, $handle2);
+	   		$batch->getDifference();
+
+	   }else{
+	   		throw new InvalidArgumentException("Could Not Process Uploaded Files. They might be corrupted");
+	   }
+	   		
+		
+
+
+
+	}else{
+			throw new InvalidArgumentException("Could Not Upload Files, please try again. <br> 
+				Please make sure both files are present.");
+	}
+
+}catch(InvalidArgumentException $e){
+	echo $e->getMessage();
+}
+
+
+class CompareBatch{
+
+	private $file1;
+	private $file2;
+	private $subscribers;
+	private $channels;
+	private $resultSet;
+
+	public function __construct(&$resource1, &$resource2, $channels = null, $subscribers = null){
+		$this->file1 = $resource1;
+		$this->file2 = $resource2;
+		$this->channels = $channels;
+		$this->subscribers = $subscribers;
+	}
+
+	public function getDifference(){
+		while($data = fgetcsv($file1,1024,",")){
+			
+		}
+	}
+}
+
+class DiffChecker
+{
+	public static function compareSubscribers($str1, $str2){
+		//cast to integers
+		$str1 = trim(str_replace(",", "", $str1), "\"");
+		$str2 = trim(str_replace(",", "", $str2), "\"");
+		$sub1 = (int)$str1;
+		$sub2 = (int)$str2;
+
+		return $sub1 == $sub2;
+	}
+
+	public static function compareChannels($str1, $str2){
+		$str1 = str_replace("http://www.youtube.com/channel/", "", $str1);
+		$str2 = str_replace("http://www.youtube.com/channel/", "", $str2);
+
+		$len1 = strlen($str1);
+		$len2 = strlen($str2);
+
+		if(abs($len1 - $len2) > 2){
+			//Strings even with UC are not equal because length difference is too much
+			return false;
+		}else{
+			if($len1 >= $len2){
+				//str1 is bigger or they are equal either way we can look for occurance
+				return strpos($str1, $str2) !== false ? true : false;
+			}
+
+			//str2 is bigger and might have UC in front of it
+			return strpos($str2, $str1) !== false ? true : false;
+		}
+
+		
+	}
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+// Just output the form
+// HACKATHON WAY
+
+?>
+
+<html>
+<head>
+	<title>CSV Difference Checker</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body class="container">
+<div class="row">
+	<header class="col-lg-12 col-md-12">
+		<h1 class="text-center">Upload Your Data Sets Below</h1>
+	</header>
+
+
+	<div class="col-lg-12 col-md-12">
+		<form id="diffCalcForm" class="form-group" method="POST" enctype= "multipart/form-data" action="CalculateDifference.php">
+			<div class="col-lg-5 form-group">
+				<input class="fileInput form-control" type="file"  name="file1" id="file1" />
+			</div>
+			<div class="col-lg-2 col-md-2 hidden-sm hidden-xs"></div>
+			<div class="col-lg-5 form-group">
+				<input class="fileInput form-control" type="file"  name="file2" id="file2" />
+			</div>
+			<div class="col-lg-12 form-group" class="options">
+				<label for="subscriber_count">Subscriber Count</label>
+				<input type="text" class="optionInput form-control" name="subscriber_count" id="subscriber_count" />
+				<label for="subscriber_count">Channel Owner</label>
+				<input type="text" class="optionInput form-control" name="channel_owner" id="channel_owner" />
+			</div>
+			<div class="col-lg-12">
+				<input type="submit" id="submitForm" class="btn btn-default" value="submit" name="submit" />
+			</div>
+		</form>
+	</div>
+
+</div>
+
+
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
